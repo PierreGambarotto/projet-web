aa16ee153f7a7aa038ddd496f1963ce5c896a891
Sylvain AUGUY <s.auguy@gmail.com>
2011-11-17 20:45:07 +0100

    test within

---
+++ spec/requests/post_delete_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'delete /post/:id' do
+
+	before(:each) do
+		@posts = [Post.create(:title => "Sujet 1", :body => "Sujet 1"),
+							Post.create(:title => "Sujet 2", :body => "Sujet 2")]
+	end
+	
+	it "affiche un lien pour supprimer un post" do
+		visit posts_path
+		@posts.each {|post| page.should have_link('Delete' , :href => post_path(post), :method => :delete)}
+	end
+
+	it "supprime le post 2, il ne reste plus que le post 1" do
+		visit posts_path
+		within ('tr', :text => @posts[2].title) do
+			click_link :Delete
+		end
+		page.should_not have_content(@post[2].body)
+	end
+
+end
--- spec/requests/post_edit_spec.rb
+++ spec/requests/post_edit_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe 'post_edit' do
   before(:each) do
-    @post = Post.create(:title => "subject1", :body => "bla bla1");
+    @posts = [Post.create(:title => "subject1", :body => "bla bla1"),
+              Post.create(:title => "subject2", :body => "bla bla2")]
+    
   end
     
 	it "affiche un lien pour editer un post" do
@@ -12,7 +14,10 @@ describe 'post_edit' do
 
 	it "renvoi la page d edition de post qui contient un formulaire" do
 		visit posts_path
-		click_link('Edit')
+    within ("tr#td", :text => @posts[2].title) do
+			click_link :Edit
+		end
+
 		page.should have_content('Edition d\'un nouveau post')
 		page.should have_selector('form')
 		page.should have_field('post_title')
@@ -20,7 +25,7 @@ describe 'post_edit' do
 	end
 
 	it "Remplissage du formulaire" do
-		visit edit_post_path(@post.id)
+		visit edit_post_path(@posts[1].id)
 		fill_in('post_title', :with => 'titre1')
 		fill_in('post_body', :with => 'contenu1')
 		click_button('Update Post')
