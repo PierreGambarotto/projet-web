23431cd8078bbe2d7f320219de1f1e308176f6be
Sylvain AUGUY <s.auguy@gmail.com>
2011-12-30 14:33:45 +0100

    controleur create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def new
-		
+		render
   end
 
   def create
+		@user = User.create(params[:user])
+		redirect_to '/'
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,10 +10,10 @@ describe UsersController do
   end
 
   describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
+     it "cree un nouvel utilisateur" do 
+			post :create, :user => {:login => "test", :email => "tester@mail.com", :password => "test123", :password_confirmation => "test123"}
+			assigns(:user).should be_a(User)
+		end
   end
 
 end
